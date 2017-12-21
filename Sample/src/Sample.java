42924b777ce49c86ee80793149d8ae2d0d160fd1
k-t-graps <ramwire5315@gmail.com>
2017-12-21 17:47:25 +0900

    12/21

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -1,19 +1,29 @@
+package pa ;
 
+class Car{
+private int num;
+private double gas;
+
+public Car(){
+	num=0;
+	gas=0.0;
+	System.out.println("車を作成しました");
+}
+public void setCar(int n,double g){
+	num=n;
+	gas=g;
+	System.out.println("ナンバーを"+num+"にガソリン量を"+gas+"にしました");
+}
+public void show(){
+	System.out.println("車のナンバーは"+num+"です");
+	System.out.println("ガソリン量は"+gas+"です");
+}
+}
 public class Sample {
 	public static void main(String [] args){
-		int[] test;
-		try{
-		test=new int[5];
-		System.out.println("tst[10]に値を代入します");
-
-		test[10]=80;
-		System.out.println("tst[10]に80を代入しました");
+	Car car1=new Car();
+			car1.show();
 
 	}
-	catch(ArrayIndexOutOfBoundsException e){
-		System.out.println("配列つの要素を超えています");
-	}
-
-}
 }
 
