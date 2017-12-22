c4ac3341ce4da1d4f259fc323cb1bd7edf1ba82d
k-t-graps <ramwire5315@gmail.com>
2017-12-22 17:41:16 +0900

    12/22

--- ecsite/ecsite/src/com/internousdev/ecsite/dao/BuyItemDAO.java
+++ ecsite/ecsite/src/com/internousdev/ecsite/dao/BuyItemDAO.java
@@ -8,40 +8,32 @@ import com.internousdev.ecsite.dto.BuyItemDTO;
 import com.internousdev.ecsite.util.DBConnector;
 
 public class BuyItemDAO {
+private DBConnector dbConnector=new DBConnector();
+private Connection connection=dbConnector.getConnection();
+private BuyItemDTO buyItemDTO=new BuyItemDTO();
 
-	private DBConnector dbConnector = new DBConnector();
+/**
+ * 商品情報取得メソッド
+ */
 
-	private Connection connection = dbConnector.getConnection();
+public BuyItemDTO getBuyItemInfo(){
+	String sql="SELECT id, item_name, item_price FROM item_info_transaction";
 
-	private BuyItemDTO buyItemDTO = new BuyItemDTO();
+	try{
+		PreparedStatement preparedStatement=connection.prepareStatement(sql);
+		ResultSet resultSet=preparedStatement.executeQuery();
 
-	/**
-	 * 商品情報取得メソッド
-	 *
-	 * @return BuyItemDTO
-	 */
-	public BuyItemDTO getBuyItemInfo() {
-
-		String sql = "SELECT id, item_name, item_price FROM item_info_transaction";
-
-		try {
-			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			ResultSet resultSet = preparedStatement.executeQuery();
-
-			if(resultSet.next()) {
-				buyItemDTO.setId(resultSet.getInt("id"));
-				buyItemDTO.setItemName(resultSet.getString("item_name"));
-				buyItemDTO.setItemPrice(resultSet.getString("item_price"));
-			}
-
-		} catch(Exception e) {
+		if(resultSet.next()){
+			buyItemDTO.setId(resultSet.getInt("id"));
+			buyItemDTO.setItemName(resultSet.getString("item_name"));
+			buyItemDTO.setItemPrice(resultSet.getString("item_price"));
+		}
+		}catch(Exception e){
 			e.printStackTrace();
 		}
-
-		return buyItemDTO;
-	}
-
-	public BuyItemDTO getBuyItemDTO() {
-		return buyItemDTO;
-	}
+	return buyItemDTO;
+}
+public BuyItemDTO getBuyItemDTO(){
+	return buyItemDTO;
+}
 }
